266d0d7bbc7742889b2a9e36c33410ee8ac9f1f9
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-04-05 09:11:46 +0300

    Added comments

--- HospitalRegistry/Models/Request.cs
+++ HospitalRegistry/Models/Request.cs
@@ -7,5 +7,7 @@ namespace HospitalRegistry.Models
 {
     public class Request
     {
+        //Patient sends a form (request) and that data should be saved in sql database. After that registry should see requests
+        //And make patients, if the patient isn't registred yet.
     }
 }
