659347da6c2692184a322f6e6f50228e9cd8f9d1
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-04-05 08:52:07 +0300

    Testing

--- HospitalRegistry/Data/PatientsInitializer.cs
+++ HospitalRegistry/Data/PatientsInitializer.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using HospitalRegistry.Models;
 
 namespace HospitalRegistry.Data
 {
     public static class PatientsInitializer
     {
-        //public static void Initialize(RegistryContext c)
-        //{
-        //    c.Database.EnsureCreated();
-        //}
+        public static void Initialize(RegistryContext c)
+        {
+            c.Database.EnsureCreated();
+            if (c.Patients.Any()) return;
+
+            //I don't understand what the following code does...
+            var cultures = CultureInfo
+                .GetCultures(CultureTypes.SpecificCultures)
+                .Select(i => new RegionInfo(i.LCID))
+                .Distinct()
+                .ToList();
+
+            //I don't understand what the following code does...
+            //var regions = cultures.OrderBy(p => p.EnglishName).ToList();
+
+            //foreach (var r in regions)
+            //{
+            //    var id = r.ThreeLetterISORegionName;
+            //    if (char.IsNumber(id[0])) continue;
+            //    var name = r.DisplayName;
+            //    var code = r.TwoLetterISORegionName;
+            //    var e = new IsoCountry(id, name, code);
+            //    c.Countries.Add(e);
+            //    c.SaveChanges();
+            //}
+        }
     }
 }
