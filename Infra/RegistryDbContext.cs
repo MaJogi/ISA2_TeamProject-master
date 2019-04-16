0810f624e41de7f420fe7f67e5166a4fae5dcbe9
Filipp Djatšuk <fidjat@ttu.ee>
2019-04-16 16:41:18 +0300

    Added entity framework, relational and DbCoetxt

---
+++ Infra/RegistryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HospitalRegistry.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra
+{
+    public class RegistryDbContext : DbContext
+    {
+        public RegistryDbContext(DbContextOptions<RegistryDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Patient>().ToTable("Patients");
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
